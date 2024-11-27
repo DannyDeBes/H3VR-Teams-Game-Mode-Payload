6b2cdeaf3273273068f4b7e3474acd5c070393f5
Packer <glennbermingham@gmail.com>
2024-11-28 02:53:24 +1100

    Initial Base

---
+++ Packer-TeamGameMode/plugin/src/TeamGameModePlugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Logging;
+using Atlas;
+using Atlas.Loaders;
+
+namespace TeamGameMode
+{
+    [BepInAutoPlugin]
+    [BepInProcess("h3vr.exe")]
+    [BepInDependency("VIP.TommySoucy.H3MP", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency(AtlasConstants.Guid, AtlasConstants.Version)]
+    public partial class TeamGameModePlugin : BaseUnityPlugin
+    {
+        public static bool h3mp = false;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+
+            AtlasPlugin.Loaders["teamgamemode"] = new SandboxLoader();
+            h3mp = Chainloader.PluginInfos.ContainsKey("VIP.TommySoucy.H3MP");
+
+
+            Logger.LogMessage($"Hello, world! Sent from {Id} {Name} {Version}");
+        }
+        
+        internal new static ManualLogSource Logger { get; private set; }
+    }
+}
